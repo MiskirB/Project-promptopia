1bafcc5cfc5bc71afbdd63f5f3f221f2c7a5436c
MiskirB <miskerbab@gmail.com>
2025-01-12 16:20:02 +0300

    nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["mongoose"],
+    serverExternalPackages: ["mongoose"], // Use the correct key
   },
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    domains: ["lh3.googleusercontent.com"], // Allow images from Google domains
   },
   webpack(config) {
     config.experiments = {
       ...config.experiments,
-      topLevelAwait: true,
+      topLevelAwait: true, // Enable top-level await
     };
     return config;
   },
 };
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//     serverComponentsExternalPackages: ["mongoose"],
+//   },
+//   images: {
+//     domains: ["lh3.googleusercontent.com"],
+//   },
+//   webpack(config) {
+//     config.experiments = {
+//       ...config.experiments,
+//       topLevelAwait: true,
+//     };
+//     return config;
+//   },
+// };
+
+// export default nextConfig;
